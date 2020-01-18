63f6acb80fdc087d780431165c6a7f86aa53a2c8
EC2 Default User <ec2-user@ip-172-31-32-78.ap-northeast-1.compute.internal>
2020-01-18 23:32:18 +0000

    createルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tweets#index'
   get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
 end
 
