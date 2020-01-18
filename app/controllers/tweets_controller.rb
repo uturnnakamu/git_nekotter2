5f61fae9597cf7906cb88cd87f53a605afc3ba15
EC2 Default User <ec2-user@ip-172-31-32-78.ap-northeast-1.compute.internal>
2020-01-18 23:35:03 +0000

    createコントローラーを作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,4 +5,14 @@ class TweetsController < ApplicationController
   
   def new 
   end
+  
+  def create
+    Tweet.create(tweet_params)
+  end
+  
+  private
+  def tweet_params
+    params.permit(:text)
+  end
+  
 end
